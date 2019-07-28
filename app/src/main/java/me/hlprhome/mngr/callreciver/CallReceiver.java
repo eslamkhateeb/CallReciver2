f62a89c13e439d576b23cd124c589c592a7b30c2
Bro <eslam.a.khateeb@gmail.com>
2019-07-28 16:48:05 +0200

    Initial commit

---
+++ app/src/main/java/me/hlprhome/mngr/callreciver/CallReceiver.java
@@ -0,0 +1,121 @@
+package me.hlprhome.mngr.callreciver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.ContentValues.TAG;
+
+public class CallReceiver extends PhonecallReceiver {
+
+    TextView tvResults;
+
+
+
+
+    @Override
+    protected void onIncomingCallReceived(Context ctx, String number, Date start)
+    {
+        //
+        Log.d("Call *****Recived","Number: " + number +" - "+ start);
+
+
+    }
+
+    @Override
+    protected void onIncomingCallAnswered(Context ctx, String number, Date start)
+    {
+        //
+        Log.d("Call Answrd","Number: "+number);
+    }
+
+    @Override
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end)
+    {
+        //
+        Log.d("Call endd","Number: "+number);
+    }
+
+    @Override
+    protected void onOutgoingCallStarted(Context ctx, String number, Date start)
+    {
+        //
+        Log.d("Call strtd","Number: "+number);
+    }
+
+    @Override
+    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end)
+    {
+        //
+        Log.d("Call Out call ended","Number: "+number);
+    }
+
+    @Override
+    protected void onMissedCall(Context ctx, String number, Date start)
+    {
+        //
+        Log.d("Call ******mssd","Number: " + number +" - "+ start);
+
+        SimpleDateFormat dateformatter = new SimpleDateFormat("dd-MM-yy");
+        SimpleDateFormat timeformatter = new SimpleDateFormat("HH:mm");
+        String dateString = dateformatter.format(start);
+        String timeString = timeformatter.format(start);
+
+        //send HTTP
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("entry.442378818", number) //number
+                .add("entry.375324555", "Missed Call") //call type
+                .add("entry.775427152", timeString) //call Duration
+                .add("entry.1136970274", dateString) //call Date
+                .build();
+
+        String url = "https://docs.google.com/forms/d/e/1FAIpQLSd28woTN9bTT_3Ks7vNqS0xXKgj4czyVecAKqK9BrIEQT9Ubg";
+
+        Request request = new Request.Builder()
+                .url(url + "/formResponse")
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+
+                    Log.d("Call ******mssd","Number: " + myResponse +" - "+ "Done");
+                    
+
+
+                }
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/me/hlprhome/mngr/callreciver/MainActivity.java
@@ -0,0 +1,55 @@
+package me.hlprhome.mngr.callreciver;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Date;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE;
+    int MY_PERMISSIONS_REQUEST_PROCESS_OUTGOING_CALLS;
+    int MY_PERMISSIONS_REQUEST_PROCESS_INTERNET;
+
+    TextView tvResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResults = (TextView) findViewById(R.id.tvResults);
+
+        if (getApplicationContext().checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted, therefore prompt the user to grant permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+        }
+
+        if (getApplicationContext().checkSelfPermission(Manifest.permission.PROCESS_OUTGOING_CALLS)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted, therefore prompt the user to grant permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.PROCESS_OUTGOING_CALLS},
+                    MY_PERMISSIONS_REQUEST_PROCESS_OUTGOING_CALLS);
+        }
+        if (getApplicationContext().checkSelfPermission(Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted, therefore prompt the user to grant permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    MY_PERMISSIONS_REQUEST_PROCESS_INTERNET);
+        }
+    }
+
+}
